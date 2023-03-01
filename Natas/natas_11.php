ab8095002f87e5bdb675f0299bc7ad628a9bd087
Vedha585 <84062017+Vedha585@users.noreply.github.com>
2023-03-01 09:54:09 +0530

    Add files via upload

---
+++ Natas/natas_11.php
@@ -0,0 +1,18 @@
+<?php  
+$data = array( "showpassword"=>"yes", "bgcolor"=>"#ffffff");  
+  
+function xor_encrypt($in) {  
+    $key = 'KNHLKNHLKNHLKNHLKNHLKNHLKNHLKNHLKNHLKNHLKN';  
+    $text = $in;  
+    $outText = '';
+    // Iterate through each character  
+    for($i=0;$i<strlen($text);$i++) {  
+    $outText .= $text[$i] ^ $key[$i % strlen($key)];  
+    }  
+  
+    return $outText;  
+}  
+  
+echo base64_encode(xor_encrypt(json_encode($data)));  
+  
+?> 
