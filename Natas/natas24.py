bd5ed9e84efbd307a51141475eb6b4f1244e7fbd
Vedha585 <84062017+Vedha585@users.noreply.github.com>
2023-03-08 09:34:57 +0530

    Update natas24.py

--- Natas/natas24.py
+++ Natas/natas24.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-# this challenge is a type juggling vulnerability
+# type juggling vulnerability
 url="http://natas24.natas.labs.overthewire.org?passwd[]=test"
 auth_user="natas24"
 auth_pass="0xzF30T9Av8lgXhW7slhFCIsVKAPyl2r"
